acdaf4c73e10336fce61a6cf5a657c25d6d67b8d
Cherin Piyumantha <cherinpiyumantha@gmail.com>
2021-05-10 23:47:22 +0530

    story section implemented

--- src/components/StoryContent.js
+++ src/components/StoryContent.js
@@ -1,7 +1,13 @@
 const StoryContent = () => {
     return (
         <div className="story__content">
-            story Content
+            <h3 className="heading-3 mb-sm">Happy Customers</h3>
+            <h2 className="heading-2 heading-2--dark mb-md">&ldquo;The best decision of our lives&rdquo;</h2>
+            <p className="story__text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Tenetur distinctio necessitatibus pariatur voluptatibus. Quidem consequatur harum volupta!
+            </p>
+            <button className="btn">Find your own home</button>
         </div>
      );
 }
--- src/components/StoryPictures.js
+++ src/components/StoryPictures.js
@@ -1,7 +1,8 @@
 const StoryPictures = () => {
     return (
         <div className="story__pictures">
-            story pictures
+            <img src="/img/story-1.jpeg" alt="story-1" className='story__img-1' />
+            <img src="/img/story-2.jpeg" alt="story-1" className='story__img-2' />
         </div>
      );
 }
