9b6e6059ccba50a377f4dd07c268969de2649f8d
Cherin Piyumantha <cherinpiyumantha@gmail.com>
2021-05-11 13:42:23 +0530

    implemented footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,20 @@
 const Footer = () => {
+    const d = new Date();
     return (
-        <footer className="footer">footer</footer>
+        <footer className="footer">
+            <ul className="nav">
+                <li className="nav__item"><a href="#" className="nav__link">Find your dream home</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Request proposal</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Download home planner</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Contact us</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Submit your property</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Come work with us!</a></li>
+            </ul>
+
+            <p className="copyright">
+                © Copyright {d.getFullYear()} by Cherin Piyumantha
+            </p>
+        </footer>
      );
 }
  
