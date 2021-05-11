19e8eb9ad9dd87195cc4ab40141f9eecd8146853
Cherin Piyumantha <cherinpiyumantha@gmail.com>
2021-05-11 19:41:57 +0530

    header implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 const Header = () => {
     return (
         <header className="header">
-            Header
+            <img src="img/logo.png" alt="logo image" className="header__logo" />
+            <h3 className="heading-3">Your own home:</h3>
+            <h1 className="heading-1">The ultimate personal freedom</h1>
+            <button className="btn header__btn">View our properties</button>
+            <div className="header__seenon-text">Seen on</div>
+
+            <div className="header__season-logos">    
+                <img src="img/logo-bbc.png" alt="img 1" />
+                <img src="img/logo-forbes.png" alt="img 1" />
+                <img src="img/logo-techcrunch.png" alt="img 1" />
+                <img src="img/logo-bi.png" alt="img 1" />
+            </div>
         </header>
      );
 }
--- src/components/Realtors.js
+++ src/components/Realtors.js
@@ -1,7 +1,28 @@
 const Realtors = () => {
     return (
         <div className="realtors">
-            Top 3 realtors
+            <h3 className="heading-3">Top 3 realtors</h3>
+            <div className="realtors__list">
+                
+                <img src="img/realtor-1.jpeg" alt="im 1" className="realtors__img" />
+                <div className="realtors__details">
+                    <h4 className="heading-4 heading-4--light">Erik Feinman</h4>
+                    <p className="realtors__sold">245 houses sold</p>
+                </div>
+
+                <img src="img/realtor-2.jpeg" alt="im 1" className="realtors__img" />
+                <div className="realtors__details">
+                    <h4 className="heading-4 heading-4--light">Kim Brown</h4>
+                    <p className="realtors__sold">212 houses sold</p>
+                </div>
+
+                <img src="img/realtor-3.jpeg" alt="im 1" className="realtors__img" />
+                <div className="realtors__details">
+                    <h4 className="heading-4 heading-4--light">Toby Ramsey</h4>
+                    <p className="realtors__sold">198 houses sold</p>
+                </div>
+
+            </div>
         </div>
      );
 }
